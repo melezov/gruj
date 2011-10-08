a82a79591151ed887f6f162fda27f544d8593314
Marko Elezovic <marko@element.hr>
2011-10-08 18:17:48 +0200

    Added java *-World examples (hw.jar)

---
+++ core/src/main/java/org/gruj/examples/GoodbyeWorld.java
@@ -0,0 +1,7 @@
+package org.gruj.examples;
+
+public final class GoodbyeWorld {
+  public static final void main(final String[] args) {
+    System.out.println("Goodbye, cruel world!");
+  }
+}
---
+++ core/src/main/java/org/gruj/examples/HelloWorld.java
@@ -0,0 +1,7 @@
+package org.gruj.examples;
+
+public final class HelloWorld {
+  public static final void main(final String[] args) {
+    System.out.println("Hello, world!");
+  }
+}
